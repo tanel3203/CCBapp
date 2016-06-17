fb433805bb93e167857156bf8cba9b51443f7918
tanel3203 <tanel3203@gmail.com>
2016-06-18 01:39:52 +0300

    made a sidebar, fixed home addItem header

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,37 @@
-// Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'firebase', 'starter.controllers', 'starter.services'])
+
+  .config(function($stateProvider,$urlRouterProvider) {
+    $stateProvider
+    .state('app', {
+    	url: '/app',
+    	abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'mainContent': {
+        templateUrl: 'templates/home.html',
+        controller: 'ListCtrl'
+      }
+    }
+  })
+
+  .state('app.browse', {
+    url: '/browse',
+    views: {
+      'mainContent': {
+        templateUrl: 'templates/browse.html'
+      }
+    }
+  })
+    $urlRouterProvider.otherwise("/app/home");
+
+
+  });
